060a74344280c872aa6145f843f2e4342a810138
Admin <suryaprakashsrivastav800@gmail.com>
2022-11-03 17:29:09 +0530

    commited for Demo Hello Controller

--- src/main/java/techi/springframework/springmicroservicedemo/SpringMicroserviceDemoApplication.java
+++ src/main/java/techi/springframework/springmicroservicedemo/SpringMicroserviceDemoApplication.java
@@ -2,12 +2,19 @@ package techi.springframework.springmicroservicedemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.Collections;
 
 @SpringBootApplication
 public class SpringMicroserviceDemoApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringMicroserviceDemoApplication.class, args);
+
+        SpringApplication app = new SpringApplication(SpringMicroserviceDemoApplication.class);
+        app.setDefaultProperties(Collections
+                .singletonMap("server.port", "6084"));
+        app.run(args);
+
+        //SpringApplication.run(MsscBreweryApplication.class, args);
     }
 
 }
