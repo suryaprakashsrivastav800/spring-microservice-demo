a36c6a9e71324de0c52142ae1d933a4649c12f63
Admin <suryaprakashsrivastav800@gmail.com>
2022-11-03 15:30:51 +0530

    commite for full code

---
+++ src/main/java/techi/springframework/springmicroservicedemo/SpringMicroserviceDemoApplication.java
@@ -0,0 +1,13 @@
+package techi.springframework.springmicroservicedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMicroserviceDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMicroserviceDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/techi/springframework/springmicroservicedemo/SpringMicroserviceDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package techi.springframework.springmicroservicedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMicroserviceDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
